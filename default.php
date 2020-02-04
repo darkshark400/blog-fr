19ba7b27e9bce133f6ee367ca78913ee1baca78d
Shuk029920 <arthurtesx@gmail.com>
2020-02-04 21:03:18 +0100

    z

--- default.php
+++ default.php
@@ -1,8 +1,12 @@
 <?php
-require_once(view/forum-acceuil.view.html);
-require_once(config/stylesheet.css);
+require_once('config/stylesheet.css');
+require_once('view/forum-acceuil.view.html');
 ?>
 <!DOCTYPE html>
 <html>
-salut
+<head>
+  <title>Blog</title>
+  <meta charset="utf-8">
+  <link href="config/style.css" rel="stylesheet">
+</head>
 </html>
