15cd97089ccfbeebcc5da6698aa8c6943af984eb
darkshark400 <51637671+darkshark400@users.noreply.github.com>
2020-02-05 21:23:10 +0100

    tes moche

--- default.php
+++ default.php
@@ -5,14 +5,15 @@
   <meta charset="utf-8" content="width=device-width" name="viewport">
   <link rel="stylesheet" type="text/css" href="config/stylesheet.css">
 </head>
-<body>
+<body id=carte>
   <h2 id=titre-h2>Bienvenue sur le blog de français</h2>
+  <br>
   <nav id=navbar>
     <div id=capteur><img class=image-capteur src='images/dots.png'/>
     <br>
     <div class=navbar-content>
       <ul>
-        <li class="text-navbar lien-navbar"><a href='view/forum-naviguer-pause.view.html'>Naviguer</a></li>
+        <li class="text-navbar lien-navbar"><a href='php/forum-naviguer-pause.php'>Naviguer</a></li>
         <li class="text-navbar lien-navbar">Publier une pause</li>
         <li class="text-navbar lien-navbar">Se connecter</li>
       </ul>
