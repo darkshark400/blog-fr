ea07e5154cf45efde652a87c061e54b289c0fc7d
Shuk029920 <arthurtesx@gmail.com>
2020-02-06 20:40:59 +0100

    zz

---
+++ php/connection.php
@@ -0,0 +1,20 @@
+<?php
+require_once('../config/connect-bdd.php');
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+      <meta charset="utf-8">
+      <link href="../config/stylesheet.css">
+      <title>Se connecter</title>
+  </head>
+
+<body>
+  <form id="connect">
+    <input type="password" value="mot de passe"/>
+  </form>
+</body>
+</html>
