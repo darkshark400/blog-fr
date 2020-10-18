5498d7f2ea7d7fc7c579d29ea716525907bfce14
Arvial <59569317+Arvialo@users.noreply.github.com>
2020-10-18 15:12:50 +0200

    Update connect-bdd.php

--- config/connect-bdd.php
+++ config/connect-bdd.php
@@ -1,3 +1,3 @@
 <?php
-$bdd = new PDO('mysql:host=db5000275456.hosting-data.io;dbname=dbs268853;charset=utf8', 'dbu441848', 'Bures!21', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+$bdd = new PDO('mysql:host='host';dbname='dbname';charset=utf8', 'user', 'password', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 ?>
